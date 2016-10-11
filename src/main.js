c86410d9a8f9f10defd66382ede53f13db95a5d4
iamstd <iamstd#163.com>
2016-10-11 23:09:44 +0800

    add

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+// import App from './App'
+// import Hello from './components/Hello'
+import Kidswant from './components/Kidswant/index.vue'
+/* eslint-disable no-new */
+new Vue({
+  el: 'body',
+  components: { Kidswant }
+})
